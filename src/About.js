4b645743c0bea4633757f73a11c8c6da7f2a647a
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-26 11:01:06 -0500

    created home about blog and photos component files

---
+++ src/About.js
@@ -0,0 +1,7 @@
+import React from "React";
+
+function About() {
+    return <h1>About</h1>;
+}
+
+export default About;
---
+++ src/Photos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Photos() {
+    return <h1>Photos</h1>;
+}
+
+export default Photos;
