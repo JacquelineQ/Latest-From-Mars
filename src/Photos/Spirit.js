b13fbb4ebbfbf006421fad47d7f7a233179da167
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-27 09:56:06 -0500

    + Spirit nav links

--- src/Photos/Spirit.js
+++ src/Photos/Spirit.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
 
 function Spirit() {
-    return <h1>Spirit</h1>;
+    return (
+        <>
+        <div className="spiritNav">
+            <Link to="spiritEarth">Spirit Earth</Link>
+            <Link to="spiritSol">Spirit Sol</Link>
+        </div>
+
+        <Outlet />
+        </>
+    )
 }
 
 export default Spirit;
