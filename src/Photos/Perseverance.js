dba818529d74511037686dacca998d2352ce2a1b
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-27 09:53:50 -0500

    + Perseverance nav links

--- src/Photos/Perseverance.js
+++ src/Photos/Perseverance.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 function Perseverance() {
-    return <h1>Perseverance</h1>;
+    return (
+        <>
+        <div className="perseveranceNav">
+            <Link to="perseveranceLatest">Perseverance Latest</Link>
+            <Link to="perseveranceEarth">Perseverance Earth</Link>
+            <Link to="perseveranceSol">Perseverance Sol</Link>
+        </div>
+
+        <Outlet />
+        </>
+    )
 }
 
 export default Perseverance;
