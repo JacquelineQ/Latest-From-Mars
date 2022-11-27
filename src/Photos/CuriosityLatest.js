3585537d8c5f19905fb6b8593466f7dab269345d
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-27 10:07:49 -0500

    added functionality for CuriosityLatest

--- src/Photos/CuriosityLatest.js
+++ src/Photos/CuriosityLatest.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import DataTable from "react-data-table-component";
 
 function CuriosityLatest() {
-    return <h1>Latest from Curiosity</h1>;
+    const [photos, setPhotos] = useState([]);
+    const [pending, setPending] = React.useState(true);
+    const [rows, setRows] = React.useState([]);
+	
+    
+
+    const columns = [
+        {
+          name: "Matian Sol",
+          selector: (row) => row.sol,
+        },
+        {
+            name: "Earth Date",
+            selector: (row) => row.earth_date,
+        },
+        {
+          name: "Camera",
+          selector: (row) => row.camera.name,
+          sortable: true
+        },
+        {
+          name: "Image",
+          cell: (props) => <a href={props.img_src} target="_blank" rel="noreferrer"><img src={props.img_src} width={60} alt="Mars"  /></a>,
+         
+        }
+      ];
+      
+    
+    useEffect(() => {
+        axios
+        .get("https://mars-photos.herokuapp.com//api/v1/rovers/curiosity/latest_photos?")
+        .then((response) => {
+            const photos = response.data.latest_photos;
+            setPhotos(photos);
+        
+              setRows(response)
+              setPending(false);
+          
+        });
+
+       
+
+    }, []);
+
+
+    
+    return (
+     
+        <div>
+        <h1>Latest Photos from the Curiosity Rover</h1>
+        <DataTable columns={columns} data={photos} progressPending={pending} pagination />
+        </div>
+    );
 }
 
 export default CuriosityLatest;
