da5d61e4d3994fb9ebc71ac1e3fe6a8c1f2c7b4f
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-27 10:11:08 -0500

    + functionality for PerseveranceLatest

--- src/Photos/PerseveranceLatest.js
+++ src/Photos/PerseveranceLatest.js
@@ -1,7 +1,58 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import DataTable from "react-data-table-component";
 
 function PerseveranceLatest() {
-    return <h1>Latest from Perseverance</h1>;
+    const [photos, setPhotos] = useState([]);
+    const [pending, setPending] = React.useState(true);
+	  const [rows, setRows] = React.useState([]);
+
+    const columns = [
+        {
+          name: "Matian Sol",
+          selector: (row) => row.sol,
+        },
+        {
+            name: "Earth Date",
+            selector: (row) => row.earth_date,
+        },
+        {
+          name: "Camera",
+          selector: (row) => row.camera.name,
+          sortable: true
+        },
+        {
+          name: "Image",
+          cell: (props) => <a href={props.img_src} target="_blank" rel="noreferrer"><img src={props.img_src} width={60} alt="Mars"  /></a>,
+         
+        }
+      ];
+      
+      useEffect(() => {
+        axios
+        .get("https://mars-photos.herokuapp.com//api/v1/rovers/perseverance/latest_photos?")
+        .then((response) => {
+            const photos = response.data.latest_photos;
+            // setPhotos(photos);
+            
+              setPhotos(photos)
+              setRows(response)
+              setPending(false);
+        });
+    }, []);
+
+    
+
+ 
+    
+    return (
+     
+        <>
+        <h1>Latest Photos from the Perseverence Rover</h1>
+        <DataTable className="dataTable" columns={columns} data={photos} progressPending={pending} pagination />
+        </>
+    );
 }
 
 export default PerseveranceLatest;
