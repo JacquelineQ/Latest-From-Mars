6b6d481fa8258d4141dce8c8bcaa5cfc0148ee2e
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-28 14:31:59 -0500

    +functionality CuriositySol, submit btn not needed

--- src/Photos/CuriositySol.js
+++ src/Photos/CuriositySol.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DataTable from "react-data-table-component";
+
+//submit button unnecessary for sol
 
 function CuriositySol() {
-    return <h1>Curiosity by Sol</h1>;
+    const [sol, setSol] = useState('');
+    const [photos, setPhotos] = useState([]);
+    const [pending, setPending] = React.useState(true);
+    const [rows, setRows] = React.useState([]);
+	
+
+    const columns = [
+        {
+            name: "Earth Date",
+            selector: (row) => row.earth_date,
+        },
+        {
+          name: "Camera",
+          selector: (row) => row.camera.name,
+          sortable: true
+        },
+        {
+          name: "Image",
+          cell: (props) => <a href={props.img_src} target="_blank" rel="noreferrer"><img src={props.img_src} width={60} alt="Mars"  /></a>,
+         
+        }
+      ];
+    const fetchPhotos = useEffect(() => {
+        axios
+        .get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=DEMO_KEY&sol=${sol}`)
+        .then((response) => {
+            const photos = response.data.photos;
+            setPhotos(photos);
+        
+              setRows(response)
+              setPending(false);
+          
+        });
+
+       
+
+    }, [sol]);
+
+    return (
+        <>
+        <form onSubmit={fetchPhotos}>
+            <label>Choose Martian Sol:</label>
+            <input 
+            type="number"
+            required
+            value={sol}
+            max="3663"
+            onChange={(e) => setSol(e.target.value)}
+            />
+        </form>
+        <div>
+        <h1>Photos from Martian Sol: {sol}</h1>
+        <DataTable columns={columns} data={photos} progressPending={pending} pagination />
+        </div>
+        </>
+    )
 }
 
 export default CuriositySol;
