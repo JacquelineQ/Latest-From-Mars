32c92730873d0e22c528c9fec7c4399acd8da749
Jacqueline <quackenbushjacqueline@gmail.com>
2022-11-29 15:24:58 -0500

    reset test

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="mainContainer">
      <h1>About</h1>
-     <p className="mainP">Hello there! Thanks so much for visiting! This site is still currently under construction
+     <p className="mainP">Hello there! Welcome to the about page! Thanks so much for visiting! This site is still currently under construction
         while we prepare some awesome content for our fellow space nerds. Please visit the "Photos"
         section where you can search through NASA's entire catalogue of photos taken by rovers Perseverance,
         Curiosity, Opportunity and Spirit. Perseverance and Curiosity are updated DAILY!
